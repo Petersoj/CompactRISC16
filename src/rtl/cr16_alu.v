7bd5332d324d7ad2dd1595e7a4b4b820ffc28ee9
Petersoj <Petersoj@users.noreply.github.com>
2021-09-02 15:26:49 -0600

    Ignore project files, setup file tree with CR16 ALU start

---
+++ src/rtl/cr16_alu.v
@@ -0,0 +1,11 @@
+////
+//
+// University of Utah, Computer Design Laboratory ECE 3710, CompactRISC16
+//
+// Create Date: 09/02/2021
+// Module Name: cr16_alu
+// Description: The CR16 ALU
+// Authors: Jacob Peterson, Brady Hartog, Isabella Gilman, Nate Hansen
+//
+////
+
---
+++ src/tb/cr16_alu_tb.v
@@ -0,0 +1,11 @@
+////
+//
+// University of Utah, Computer Design Laboratory ECE 3710, CompactRISC16
+//
+// Create Date: 09/02/2021
+// Module Name: cr16_alu_tb
+// Description: The CR16 ALU testbench
+// Authors: Jacob Peterson, Brady Hartog, Isabella Gilman, Nate Hansen
+//
+////
+
