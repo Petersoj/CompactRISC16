47f05e011647ea3bf403fb36f3670e15fa995e79
Jacob Peterson <Petersoj@users.noreply.github.com>
2021-09-07 18:01:29 -0600

    Add ALU port list and parameterized opcodes and status

--- src/rtl/cr16_alu.v
+++ src/rtl/cr16_alu.v
@@ -7,3 +7,40 @@
 // Authors: Jacob Peterson, Brady Hartog, Isabella Gilman, Nate Hansen
 //
 
+module alu #(parameter integer P_WIDTH = 16)
+            (input wire I_CLK,
+             input wire I_ENABLE,
+             input wire [3 : 0] I_OPCODE,
+             input wire [P_WIDTH - 1 : 0] I_A,
+             input wire [P_WIDTH - 1 : 0] I_B,
+             output reg [P_WIDTH - 1 : 0] O_C,
+             output reg [4 : 0] O_STATUS);
+ 
+    // Parameterized opcodes
+    parameter integer
+    ADD = 0,
+    ADDU = 1,
+    ADDC = 2,
+    ADDCU = 3,
+    SUB = 4,
+    CMP = 5,
+    CMPU = 6,
+    AND = 7,
+    OR = 8,
+    XOR = 9,
+    NOT = 10,
+    LSH = 11,
+    RSH = 12,
+    ALSH = 13,
+    ARSH = 14,
+    NOP = 15;
+    
+    // Parameterized status outputs
+    parameter integer
+    STATUS_CARRY = 5'b00001,
+    STATUS_LOW = 5'b00010,
+    STATUS_FLAG = 5'b00100,
+    STATUS_ZERO = 5'b01000,
+    STATUS_NEGATIVE = 5'b10000;
+
+endmodule
