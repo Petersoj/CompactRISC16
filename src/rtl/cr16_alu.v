55dd1f73ec0f9f245838c27f6a1b584062a761a5
Jacob Peterson <Petersoj@users.noreply.github.com>
2021-09-09 19:54:17 -0600

    Format src/ files via .formatter/verilog/format shell script

--- src/rtl/cr16_alu.v
+++ src/rtl/cr16_alu.v
@@ -8,135 +8,135 @@
 //
 
 module cr16_alu #(parameter integer P_WIDTH = 16)
-            (input wire I_CLK,
-             input wire I_ENABLE,
-             input wire [3 : 0] I_OPCODE,
-             input wire [P_WIDTH - 1 : 0] I_A,
-             input wire [P_WIDTH - 1 : 0] I_B,
-             output reg [P_WIDTH - 1 : 0] O_C,
-             output reg [4 : 0] O_STATUS);
+       (input wire I_CLK,
+        input wire I_ENABLE,
+        input wire [3 : 0] I_OPCODE,
+        input wire [P_WIDTH - 1 : 0] I_A,
+        input wire [P_WIDTH - 1 : 0] I_B,
+        output reg [P_WIDTH - 1 : 0] O_C,
+        output reg [4 : 0] O_STATUS);
 
-    // Parameterized Opcodes
-    localparam integer
-    ADD = 0,
-    ADDU = 1,
-    ADDC = 2,
-    ADDCU = 3,
-    SUB = 4,
-    CMP = 5,
-    CMPU = 6,
-    AND = 7,
-    OR = 8,
-    XOR = 9,
-    NOT = 10,
-    LSH = 11,
-    RSH = 12,
-    ALSH = 13,
-    ARSH = 14;
+// Parameterized Opcodes
+localparam integer
+           ADD = 0,
+           ADDU = 1,
+           ADDC = 2,
+           ADDCU = 3,
+           SUB = 4,
+           CMP = 5,
+           CMPU = 6,
+           AND = 7,
+           OR = 8,
+           XOR = 9,
+           NOT = 10,
+           LSH = 11,
+           RSH = 12,
+           ALSH = 13,
+           ARSH = 14;
 
-    // Status register indicies for one-hot encoding
-    localparam integer
-    STATUS_INDEX_CARRY = 0,
-    STATUS_INDEX_LOW = 1,
-    STATUS_INDEX_FLAG = 2,
-    STATUS_INDEX_ZERO = 3,
-    STATUS_INDEX_NEGATIVE = 4;
-    
-    // Clock block
-    always @(posedge I_CLK) begin
-        if (I_ENABLE) begin
-            case (I_OPCODE)
-                ADD: begin
-                    // Add 'I_A' and 'I_B' and set to 'O_C'
-                    O_C = I_A + I_B;
-                    // Do not set the Carry status bit for signed arithmetic
-                    O_STATUS[STATUS_INDEX_CARRY] = 0;
-                    // Do not set the Low status bit for signed arithmetic
-                    O_STATUS[STATUS_INDEX_LOW] = 0;
-                    // Set the Flag status bit for signed carry overflow (this occurs when the MSB
-                    // of the result is flipped compared to the MSB of the operands)
-                    O_STATUS[STATUS_INDEX_FLAG] = 
-                        (~I_A[P_WIDTH - 1] & ~I_B[P_WIDTH - 1] & O_C[P_WIDTH - 1]) | 
-                        (I_A[P_WIDTH - 1] & I_B[P_WIDTH - 1] & ~O_C[P_WIDTH - 1]);
-                    // Set the Zero status bit if sum is 0
-                    O_STATUS[STATUS_INDEX_ZERO] = O_C == 0; 
-                    // Set the Negative status bit if result is negative (sign bit is 1)
-                    O_STATUS[STATUS_INDEX_NEGATIVE] = O_C[P_WIDTH - 1] == 1'b1;
-                end
-                ADDU: begin
-                    // Add 'I_A' and 'I_B' and set to 'O_C' with carry status bit specified
-                    {O_STATUS[STATUS_INDEX_CARRY], O_C} = I_A + I_B;
-                    O_STATUS[STATUS_INDEX_LOW] = 0;
-                    O_STATUS[STATUS_INDEX_FLAG] = 0;
-                    O_STATUS[STATUS_INDEX_ZERO] = O_C == 0;
-                    O_STATUS[STATUS_INDEX_NEGATIVE] = 0;
-                end
-                ADDC: begin
-                    // Add 'I_A' and 'I_B' and 1 and set to 'O_C'
-                    // This instruction is needed due to adding 'immediate high' numbers
-                    // in the event that an 'immediate low' addition instruction caused a carry.
-                    O_C = I_A + I_B + 1'b1;
-                    O_STATUS[STATUS_INDEX_CARRY] = 0;
-                    O_STATUS[STATUS_INDEX_LOW] = 0;
-                    O_STATUS[STATUS_INDEX_FLAG] =
-                        (~I_A[P_WIDTH - 1] & ~I_B[P_WIDTH - 1] & O_C[P_WIDTH - 1]) |
-                        (I_A[P_WIDTH - 1] & I_B[P_WIDTH - 1] & ~O_C[P_WIDTH - 1]);
-                    O_STATUS[STATUS_INDEX_ZERO] = O_C == 0;
-                    O_STATUS[STATUS_INDEX_NEGATIVE] = O_C[P_WIDTH - 1] == 1'b1;
-                end
-                ADDCU: begin
-                    // Add 'I_A' and 'I_B' and 1 and set to 'O_C' with carry status bit specified
-                    {O_STATUS[STATUS_INDEX_CARRY], O_C} = I_A + I_B + 1;
-                    O_STATUS[STATUS_INDEX_LOW] = 0;
-                    O_STATUS[STATUS_INDEX_FLAG] = 0;
-                    O_STATUS[STATUS_INDEX_ZERO] = O_C == 0;
-                    O_STATUS[STATUS_INDEX_NEGATIVE] = 0;
-                end
-                SUB: begin
-                    // Subtract 'I_A' from 'I_B' and set to 'O_C'
-                    O_C = I_B - I_A;
-                    O_STATUS[STATUS_INDEX_CARRY] = 0;
-                    O_STATUS[STATUS_INDEX_LOW] = 0;
-                    O_STATUS[STATUS_INDEX_FLAG] =
-                        (~I_A[P_WIDTH - 1] & ~I_B[P_WIDTH - 1] & O_C[P_WIDTH - 1]) |
-                        (I_A[P_WIDTH - 1] & I_B[P_WIDTH - 1] & ~O_C[P_WIDTH - 1]);
-                    O_STATUS[STATUS_INDEX_ZERO] = O_C == 0;
-                    O_STATUS[STATUS_INDEX_NEGATIVE] = O_C[P_WIDTH - 1] == 1'b1;
-                end
-                CMP: begin
-                    
-                end
-                CMPU: begin
-                    
-                end
-                AND: begin
-                    
-                end
-                OR: begin
-                    
-                end
-                XOR: begin
-                    
-                end
-                NOT: begin
-                    
-                end
-                LSH: begin
-                    
-                end
-                RSH: begin
-                    
-                end
-                ALSH: begin
-                    
-                end
-                ARSH: begin
-                    
-                end
-                default: begin
-                    
-                end
-            endcase
-        end
+// Status register indicies for one-hot encoding
+localparam integer
+           STATUS_INDEX_CARRY = 0,
+           STATUS_INDEX_LOW = 1,
+           STATUS_INDEX_FLAG = 2,
+           STATUS_INDEX_ZERO = 3,
+           STATUS_INDEX_NEGATIVE = 4;
+
+// Clock block
+always @(posedge I_CLK) begin
+    if (I_ENABLE) begin
+        case (I_OPCODE)
+            ADD: begin
+                // Add 'I_A' and 'I_B' and set to 'O_C'
+                O_C = I_A + I_B;
+                // Do not set the Carry status bit for signed arithmetic
+                O_STATUS[STATUS_INDEX_CARRY] = 0;
+                // Do not set the Low status bit for signed arithmetic
+                O_STATUS[STATUS_INDEX_LOW] = 0;
+                // Set the Flag status bit for signed carry overflow (this occurs when the MSB
+                // of the result is flipped compared to the MSB of the operands)
+                O_STATUS[STATUS_INDEX_FLAG] =
+                (~I_A[P_WIDTH - 1] & ~I_B[P_WIDTH - 1] & O_C[P_WIDTH - 1]) |
+                (I_A[P_WIDTH - 1] & I_B[P_WIDTH - 1] & ~O_C[P_WIDTH - 1]);
+                // Set the Zero status bit if sum is 0
+                O_STATUS[STATUS_INDEX_ZERO] = O_C == 0;
+                // Set the Negative status bit if result is negative (sign bit is 1)
+                O_STATUS[STATUS_INDEX_NEGATIVE] = O_C[P_WIDTH - 1] == 1'b1;
+            end
+            ADDU: begin
+                // Add 'I_A' and 'I_B' and set to 'O_C' with carry status bit specified
+                {O_STATUS[STATUS_INDEX_CARRY], O_C} = I_A + I_B;
+                O_STATUS[STATUS_INDEX_LOW] = 0;
+                O_STATUS[STATUS_INDEX_FLAG] = 0;
+                O_STATUS[STATUS_INDEX_ZERO] = O_C == 0;
+                O_STATUS[STATUS_INDEX_NEGATIVE] = 0;
+            end
+            ADDC: begin
+                // Add 'I_A' and 'I_B' and 1 and set to 'O_C'
+                // This instruction is needed due to adding 'immediate high' numbers
+                // in the event that an 'immediate low' addition instruction caused a carry.
+                O_C = I_A + I_B + 1'b1;
+                O_STATUS[STATUS_INDEX_CARRY] = 0;
+                O_STATUS[STATUS_INDEX_LOW] = 0;
+                O_STATUS[STATUS_INDEX_FLAG] =
+                (~I_A[P_WIDTH - 1] & ~I_B[P_WIDTH - 1] & O_C[P_WIDTH - 1]) |
+                (I_A[P_WIDTH - 1] & I_B[P_WIDTH - 1] & ~O_C[P_WIDTH - 1]);
+                O_STATUS[STATUS_INDEX_ZERO] = O_C == 0;
+                O_STATUS[STATUS_INDEX_NEGATIVE] = O_C[P_WIDTH - 1] == 1'b1;
+            end
+            ADDCU: begin
+                // Add 'I_A' and 'I_B' and 1 and set to 'O_C' with carry status bit specified
+                {O_STATUS[STATUS_INDEX_CARRY], O_C} = I_A + I_B + 1;
+                O_STATUS[STATUS_INDEX_LOW] = 0;
+                O_STATUS[STATUS_INDEX_FLAG] = 0;
+                O_STATUS[STATUS_INDEX_ZERO] = O_C == 0;
+                O_STATUS[STATUS_INDEX_NEGATIVE] = 0;
+            end
+            SUB: begin
+                // Subtract 'I_A' from 'I_B' and set to 'O_C'
+                O_C = I_B - I_A;
+                O_STATUS[STATUS_INDEX_CARRY] = 0;
+                O_STATUS[STATUS_INDEX_LOW] = 0;
+                O_STATUS[STATUS_INDEX_FLAG] =
+                (~I_A[P_WIDTH - 1] & ~I_B[P_WIDTH - 1] & O_C[P_WIDTH - 1]) |
+                (I_A[P_WIDTH - 1] & I_B[P_WIDTH - 1] & ~O_C[P_WIDTH - 1]);
+                O_STATUS[STATUS_INDEX_ZERO] = O_C == 0;
+                O_STATUS[STATUS_INDEX_NEGATIVE] = O_C[P_WIDTH - 1] == 1'b1;
+            end
+            CMP: begin
+
+            end
+            CMPU: begin
+
+            end
+            AND: begin
+
+            end
+            OR: begin
+
+            end
+            XOR: begin
+
+            end
+            NOT: begin
+
+            end
+            LSH: begin
+
+            end
+            RSH: begin
+
+            end
+            ALSH: begin
+
+            end
+            ARSH: begin
+
+            end
+            default: begin
+
+            end
+        endcase
     end
+end
 endmodule
--- src/tb/tb_cr16_alu.v
+++ src/tb/tb_cr16_alu.v
@@ -8,84 +8,84 @@
 //
 
 module tb_cr16_alu();
-    
-    // Inputs
-    reg [15:0] I_A;
-    reg [15:0] I_B;
-    reg [3:0] I_OPCODE;
-	 reg I_ENABLE;
-	 reg I_CLK;
-    
-    // Outputs
-    wire [15:0] O_C;
-    wire [4:0] O_STATUS;
-    
-	 // establish the clock signal to sync the test
-	 always #5 I_CLK = ~I_CLK;
-	 
-    integer i, j;
-    // Instantiate the Unit Under Test (UUT)
-    alu uut (
-	 .I_CLK(I_CLK),
-	 .I_ENABLE(I_ENABLE),
-    .I_A(I_A),
-    .I_B(I_B),
-    .O_C(O_C),
-    .I_OPCODE(I_OPCODE),
-    .O_STATUS(O_STATUS)
+
+// Inputs
+reg [15:0] I_A;
+reg [15:0] I_B;
+reg [3:0] I_OPCODE;
+reg I_ENABLE;
+reg I_CLK;
+
+// Outputs
+wire [15:0] O_C;
+wire [4:0] O_STATUS;
+
+// establish the clock signal to sync the test
+always #5 I_CLK = ~I_CLK;
+
+integer i, j;
+// Instantiate the Unit Under Test (UUT)
+alu uut (
+        .I_CLK(I_CLK),
+        .I_ENABLE(I_ENABLE),
+        .I_A(I_A),
+        .I_B(I_B),
+        .O_C(O_C),
+        .I_OPCODE(I_OPCODE),
+        .O_STATUS(O_STATUS)
     );
-    
-    initial begin
-        //			$monitor("I_op1 %0d, I_op2: %0d, O_dest: %0d, flags[4:0]:%b, time:%0d", I_op1, I_op2, O_dest, flags[4:0], $time);
-        //Instead of the $display stmt in the loop, you could use just this
-        //monitor statement which is executed everytime there is an event on any
-        //signal in the argument list.
-        
-        // Initialize Inputs
-        I_A  = 0;
-        I_B  = 0;
-        I_ENABLE = 1'b1;
-        
-        // Wait 100 ns for global reset to finish
-        /*****
-         // One vector-by-vector case simulation
-         #10;
-         Opcode = 2'b11;
-         I_op1  = 4'b0010; I_op2  = 4'b0011;
-         #10
-         I_op1 = 4'b1111; I_op2 = 4'b 1110;
-         //$display("I_op1: %b, I_op2: %b, O_dest:%b, flags[4:0]: %b, time:%d", I_op1, I_op2, O_dest, flags[4:0], $time);
-         ****/
-			
-			
-        //Simulate ADD, Opcode = 0
-		  I_OPCODE = 4'b0000;
-		  
-        for(i = -32_768; i < 32_767; i = i + 1) begin            
-            I_A = i;
-				
-				for(j = -32_768; j < 32_767; j = j + 1) begin
-					I_B = j;
-					#10;
-					
-					if (O_C != i + j)
-						$display("Test Failed: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
-					if ((~I_A[15] & ~I_B[15] & O_C[15]) | (I_A[15] & I_B[15] & ~O_C[15]) && (O_STATUS[2] != 1'b1))
-						$display("Signed Overflow not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
-					if ( O_C == 0 && O_STATUS[3] != 1)
-						$display("Zero bit not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
-					if ( O_C[3] == 1 && O_STATUS[4] != 1)
-						$display("Neg bit not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
-					if (I_B < I_A && O_STATUS[1] != 1)
-						$display("Low bit not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
-					if (O_STATUS[0] == 1)
-						$display("Carry bit set incorrectly: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
-			   end
-		  end
-        //$finish(2);
-        
-        // Add stimulus here
-        
+
+initial begin
+    //   $monitor("I_op1 %0d, I_op2: %0d, O_dest: %0d, flags[4:0]:%b, time:%0d", I_op1, I_op2, O_dest, flags[4:0], $time);
+    //Instead of the $display stmt in the loop, you could use just this
+    //monitor statement which is executed everytime there is an event on any
+    //signal in the argument list.
+
+    // Initialize Inputs
+    I_A  = 0;
+    I_B  = 0;
+    I_ENABLE = 1'b1;
+
+    // Wait 100 ns for global reset to finish
+    /*****
+     // One vector-by-vector case simulation
+     #10;
+     Opcode = 2'b11;
+     I_op1  = 4'b0010; I_op2  = 4'b0011;
+     #10
+     I_op1 = 4'b1111; I_op2 = 4'b 1110;
+     //$display("I_op1: %b, I_op2: %b, O_dest:%b, flags[4:0]: %b, time:%d", I_op1, I_op2, O_dest, flags[4:0], $time);
+     ****/
+
+
+    //Simulate ADD, Opcode = 0
+    I_OPCODE = 4'b0000;
+
+    for(i = -32_768; i < 32_767; i = i + 1) begin
+        I_A = i;
+
+        for(j = -32_768; j < 32_767; j = j + 1) begin
+            I_B = j;
+            #10;
+
+            if (O_C != i + j)
+                $display("Test Failed: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
+            if ((~I_A[15] & ~I_B[15] & O_C[15]) | (I_A[15] & I_B[15] & ~O_C[15]) && (O_STATUS[2] != 1'b1))
+                $display("Signed Overflow not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
+            if ( O_C == 0 && O_STATUS[3] != 1)
+                $display("Zero bit not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
+            if ( O_C[3] == 1 && O_STATUS[4] != 1)
+                $display("Neg bit not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
+            if (I_B < I_A && O_STATUS[1] != 1)
+                $display("Low bit not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
+            if (O_STATUS[0] == 1)
+                $display("Carry bit set incorrectly: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
+        end
     end
-    
+    //$finish(2);
+
+    // Add stimulus here
+
+end
+
 endmodule
