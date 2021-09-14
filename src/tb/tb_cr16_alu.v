83d11a64152b255a71db5b9b30c28348f46aa062
Nate Hansen <77819611+nateute@users.noreply.github.com>
2021-09-14 15:49:42 -0600

    Added ADDU to tb

--- src/tb/tb_cr16_alu.v
+++ src/tb/tb_cr16_alu.v
@@ -1,3 +1,4 @@
+`timescale 1ps/1ps
 //
 // University of Utah, Computer Design Laboratory ECE 3710, CompactRISC16
 //
@@ -21,11 +22,12 @@ wire [15:0] O_C;
 wire [4:0] O_STATUS;
 
 // establish the clock signal to sync the test
-always #5 I_CLK = ~I_CLK;
+always #1 I_CLK = ~I_CLK;
 
 integer i, j;
+integer k, l;
 // Instantiate the Unit Under Test (UUT)
-alu uut (
+cr16_alu uut (
         .I_CLK(I_CLK),
         .I_ENABLE(I_ENABLE),
         .I_A(I_A),
@@ -45,6 +47,7 @@ initial begin
     I_A  = 0;
     I_B  = 0;
     I_ENABLE = 1'b1;
+	 I_CLK = 0;
 
     // Wait 100 ns for global reset to finish
     /*****
@@ -59,6 +62,7 @@ initial begin
 
 
     //Simulate ADD, Opcode = 0
+	 /*
     I_OPCODE = 4'b0000;
 
     for(i = -32_768; i < 32_767; i = i + 1) begin
@@ -66,20 +70,49 @@ initial begin
 
         for(j = -32_768; j < 32_767; j = j + 1) begin
             I_B = j;
-            #10;
+            #2;
 
-            if (O_C != i + j)
-                $display("Test Failed: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
+            if ($signed(O_C) != $signed(I_A) + $signed(I_B))
+                $display("Test Failed: I_A: %0d, I_B: %0d, i:%0d, j%0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, i, j, O_C, O_STATUS[4:0]);
+				
             if ((~I_A[15] & ~I_B[15] & O_C[15]) | (I_A[15] & I_B[15] & ~O_C[15]) && (O_STATUS[2] != 1'b1))
                 $display("Signed Overflow not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
             if ( O_C == 0 && O_STATUS[3] != 1)
                 $display("Zero bit not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
-            if ( O_C[3] == 1 && O_STATUS[4] != 1)
+            if ( O_C[15] == 1 && O_STATUS[4] != 1)
                 $display("Neg bit not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
-            if (I_B < I_A && O_STATUS[1] != 1)
-                $display("Low bit not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
+				//// Decide here if we really want to set the low bit or not.
+            if ($signed(I_B) < $signed(I_A) && O_STATUS[1] != 1);
+                //$display("Low bit not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
             if (O_STATUS[0] == 1)
                 $display("Carry bit set incorrectly: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
+        end
+    end
+	 */
+	 //Simulate ADDU, Opcode = 1
+    I_OPCODE = 4'b0001;
+
+    for(k = 0; k < 65_535; k = k + 1) begin
+        I_A = k;
+
+        for(l = 0; l < 65_535; l = l + 1) begin
+            I_B = l;
+            #2;
+
+            if (O_C != I_A + I_B)
+                $display("Test Failed: I_A: %0d, I_B: %0d, i:%0d, j%0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, i, j, O_C, O_STATUS[4:0]);
+				
+            if (O_STATUS[2] == 1'b1)
+                $display("Signed Overflow set incorrectly: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
+            if ( O_C == 0 && O_STATUS[3] != 1)
+                $display("Zero bit not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
+            //if ( O_C[15] == 1 && O_STATUS[4] == 1)
+                //$display("Neg bit not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
+				//// Decide here if we really want to set the low bit or not.
+            if ($signed(I_B) < $signed(I_A) && O_STATUS[1] != 1);
+                //$display("Low bit not set: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
+            if (O_STATUS[1] == 0);
+                //$display("Carry bit set incorrectly: I_A: %0d, I_B: %0d, O_C: %0d, flags[4:0]: %b", I_A, I_B, O_C, O_STATUS[4:0]);
         end
     end
     //$finish(2);
