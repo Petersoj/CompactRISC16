40c60a33db437a1fc974dfb6c4166fdcffa40841
Jacob Peterson <Petersoj@users.noreply.github.com>
2021-09-16 16:51:26 -0600

    Change port names in seven_segment_bcd_mapping module

--- src/rtl/seven_segment_bcd_mapping.v
+++ src/rtl/seven_segment_bcd_mapping.v
@@ -14,41 +14,41 @@ module bcd_seven_segment_mapping
         output reg [6:0] O_SEVEN_SEGMENT);
 
 always @(*) begin
-    case (digit_in)
+    case (I_BCD)
         4'b0000:
-            display_digit = 7'b1111110; // 0
+            O_SEVEN_SEGMENT = 7'b1111110; // 0
         4'b0001:
-            display_digit = 7'b0110000; // 1
+            O_SEVEN_SEGMENT = 7'b0110000; // 1
         4'b0010:
-            display_digit = 7'b1101101; // 2
+            O_SEVEN_SEGMENT = 7'b1101101; // 2
         4'b0011:
-            display_digit = 7'b1111001; // 3
+            O_SEVEN_SEGMENT = 7'b1111001; // 3
         4'b0100:
-            display_digit = 7'b0110011; // 4
+            O_SEVEN_SEGMENT = 7'b0110011; // 4
         4'b0101:
-            display_digit = 7'b1011011; // 5
+            O_SEVEN_SEGMENT = 7'b1011011; // 5
         4'b0110:
-            display_digit = 7'b1011111; // 6
+            O_SEVEN_SEGMENT = 7'b1011111; // 6
         4'b0111:
-            display_digit = 7'b1110000; // 7
+            O_SEVEN_SEGMENT = 7'b1110000; // 7
         4'b1000:
-            display_digit = 7'b1111111; // 8
+            O_SEVEN_SEGMENT = 7'b1111111; // 8
         4'b1001:
-            display_digit = 7'b1111011; // 9
+            O_SEVEN_SEGMENT = 7'b1111011; // 9
         4'b1010:
-            display_digit = 7'b1110111; // A
+            O_SEVEN_SEGMENT = 7'b1110111; // A
         4'b1011:
-            display_digit = 7'b0011111; // B
+            O_SEVEN_SEGMENT = 7'b0011111; // B
         4'b1100:
-            display_digit = 7'b0001101; // C
+            O_SEVEN_SEGMENT = 7'b0001101; // C
         4'b1101:
-            display_digit = 7'b0111101; // D
+            O_SEVEN_SEGMENT = 7'b0111101; // D
         4'b1110:
-            display_digit = 7'b1001111; // E
+            O_SEVEN_SEGMENT = 7'b1001111; // E
         4'b1111:
-            display_digit = 7'b1000111; // F
+            O_SEVEN_SEGMENT = 7'b1000111; // F
         default:
-            display_digit = 7'b0000000;
+            O_SEVEN_SEGMENT = 7'b0000000;
     endcase
 end
 endmodule
