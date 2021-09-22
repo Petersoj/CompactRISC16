9cdc84a50ca9acdaad091a172cae78956f5f6864
Nate Hansen <77819611+nateute@users.noreply.github.com>
2021-09-21 20:14:16 -0600

    added top module datapath, added decoder.v - WIP

---
+++ src/rtl/encoder.v
@@ -0,0 +1,20 @@
+// University of Utah, Computer Design Laboratory ECE 3710, CompactRISC16
+//
+// Create Date: 09/21/2021
+// Module Name: encoder
+// Description: Top level module for the combination of the datapath with the alu.
+// Authors: Jacob Peterson, Brady Hartog, Isabella Gilman, Nate Hansen
+//
+
+module ( input wire [15:0] REG_ENABLE,
+			input wire I_ENABLE,
+			output reg [3:0] O_S			
+		);
+		
+		always@(I_ENABLE) begin
+			case(REG_ENABLE)
+		
+		
+		
+endmodule
+	
