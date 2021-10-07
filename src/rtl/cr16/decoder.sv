32b86e36a91cc5ef7d4f6c98c02ff0bbe59400dd
Nate Hansen <77819611+nateute@users.noreply.github.com>
2021-10-07 17:29:41 -0600

    Finish PC, delete decoder

--- src/rtl/cr16/decoder.sv
+++
@@ -1,9 +0,0 @@
-//
-// University of Utah, Computer Design Laboratory ECE 3710, CompactRISC16
-//
-// Create Date: 10/7/2021
-// Module Name: decoder
-// Description: 
-// Authors: Jacob Peterson, Brady Hartog, Isabella Gilman, Nate Hansen
-//
-
--- src/rtl/cr16/pc.sv
+++ src/rtl/cr16/pc.sv
@@ -6,3 +6,34 @@
 // Description: Program counter to drive the FSM of the CR16 ALU. 
 // Authors: Jacob Peterson, Brady Hartog, Isabella Gilman, Nate Hansen
 //
+
+module pc #(parameter integer P_ADDRESS_WIDTH = 16)( input wire I_CLK,
+           input wire I_ENABLE,			  
+           input wire I_NRESET,
+			  input wire [P_ADDRESS_WIDTH - 1:0]I_ADDRESS,
+			  input wire I_ADDRESS_SELECT,			  
+			  output reg [P_ADDRESS_WIDTH - 1:0]O_ADDRESS);
+			  
+			  
+	 always@(posedge I_CLK) begin
+	  
+	   if (I_ENABLE) begin
+			if(!I_NRESET)
+				O_ADDRESS = 0;
+		  
+			else begin
+				if (I_ADDRESS_SELECT)
+					O_ADDRESS = I_ADDRESS;
+			
+				else
+					O_ADDRESS += 1;
+			end
+		end	
+
+		else
+			O_ADDRESS = O_ADDRESS;
+	 end
+	 
+	 
+endmodule
+
