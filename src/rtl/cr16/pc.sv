0f6789fa123bd0309d9fd93837d09af71a0f9570
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2021-10-07 17:54:46 -0600

    Format pc.sv

--- src/rtl/cr16/pc.sv
+++ src/rtl/cr16/pc.sv
@@ -3,37 +3,38 @@
 //
 // Create Date: 10/7/2021
 // Module Name: pc
-// Description: Program counter to drive the FSM of the CR16 ALU. 
+// Description: Program counter to drive the FSM of the CR16 ALU.
 // Authors: Jacob Peterson, Brady Hartog, Isabella Gilman, Nate Hansen
 //
 
-module pc #(parameter integer P_ADDRESS_WIDTH = 16)( input wire I_CLK,
-           input wire I_ENABLE,			  
-           input wire I_NRESET,
-			  input wire [P_ADDRESS_WIDTH - 1:0]I_ADDRESS,
-			  input wire I_ADDRESS_SELECT,			  
-			  output reg [P_ADDRESS_WIDTH - 1:0]O_ADDRESS);
-			  
-			  
-	 always@(posedge I_CLK) begin
-	  
-	   if (I_ENABLE) begin
-			if(!I_NRESET)
-				O_ADDRESS = 0;
-		  
-			else begin
-				if (I_ADDRESS_SELECT)
-					O_ADDRESS = I_ADDRESS;
-			
-				else
-					O_ADDRESS += 1;
-			end
-		end	
+// @param P_ADDRESS_WIDTH  the width of the input/output address
+// @param I_CLK            the clock signal
+// @param I_ENABLE         the enable signal
+// @param I_NRESET         the active-low reset signal
+// @param I_ADDRESS        the address input value
+// @param I_ADDRESS_SELECT assert to output 'I_ADDRESS' on 'O_ADDRESS' or reset to increment and
+//                         output 'O_ADDRESS' on every posedge of 'I_CLK'
+// @param O_ADDRESS        the address output value
+module pc #(parameter integer P_ADDRESS_WIDTH = 16)
+       (input wire I_CLK,
+        input wire I_ENABLE,
+        input wire I_NRESET,
+        input wire [P_ADDRESS_WIDTH - 1 : 0] I_ADDRESS,
+        input wire I_ADDRESS_SELECT,
+        output reg [P_ADDRESS_WIDTH - 1 : 0] O_ADDRESS);
 
-		else
-			O_ADDRESS = O_ADDRESS;
-	 end
-	 
-	 
+always@(posedge I_CLK) begin
+    if (I_ENABLE) begin
+        if(!I_NRESET)
+            O_ADDRESS = 0;
+        else begin
+            if (I_ADDRESS_SELECT)
+                O_ADDRESS = I_ADDRESS;
+            else
+                O_ADDRESS += 1;
+        end
+    end
+    else
+        O_ADDRESS = O_ADDRESS;
+end
 endmodule
-
