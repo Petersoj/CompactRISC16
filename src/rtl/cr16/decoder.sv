44ea8e7aabec43828648355d9a48a4375988f8bc
Nate Hansen <77819611+nateute@users.noreply.github.com>
2021-10-07 16:21:24 -0600

    File creation for decoder and program counter

---
+++ src/rtl/cr16/decoder.sv
@@ -0,0 +1,9 @@
+//
+// University of Utah, Computer Design Laboratory ECE 3710, CompactRISC16
+//
+// Create Date: 10/7/2021
+// Module Name: decoder
+// Description: 
+// Authors: Jacob Peterson, Brady Hartog, Isabella Gilman, Nate Hansen
+//
+
---
+++ src/rtl/cr16/pc.sv
@@ -0,0 +1,8 @@
+//
+// University of Utah, Computer Design Laboratory ECE 3710, CompactRISC16
+//
+// Create Date: 10/7/2021
+// Module Name: pc
+// Description: Program counter to drive the FSM of the CR16 ALU. 
+// Authors: Jacob Peterson, Brady Hartog, Isabella Gilman, Nate Hansen
+//
