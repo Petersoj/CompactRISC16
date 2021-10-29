35af1bdc62e16a0af0a2549ef278963eb65c35ee
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2021-10-29 02:13:06 -0600

    Fix/format asm and cr16_top_init bram_init dat files

--- resources/asm/test1_add_fibonacci.asm
+++
@@ -1,8 +0,0 @@
-ADDI    r0, 1,
-ADDI    r1, 1,
-ADD     r1, r0  # sum 2
-ADD     r0, r1  # sum 3
-ADD     r1, r0  # sum 5
-ADD     r0, r1  # sum 8
-ADD     r1, r0  # sum 13
-ADD     r0, r1  # sum 21
