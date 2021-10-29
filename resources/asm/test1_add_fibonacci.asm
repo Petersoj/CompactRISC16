5e254552c63d8156a23fab5fd6ac2d87fb3243b9
Brady Hartog <bjhartog@me.com>
2021-10-28 18:17:42 -0600

    Add first two assembly programs

---
+++ resources/asm/test1_add_fibonacci.asm
@@ -0,0 +1,8 @@
+ADDI    r0, 1,
+ADDI    r1, 1,
+ADD     r1, r0  # sum 2
+ADD     r0, r1  # sum 3
+ADD     r1, r0  # sum 5
+ADD     r0, r1  # sum 8
+ADD     r1, r0  # sum 13
+ADD     r0, r1  # sum 21
---
+++ resources/asm/test2_mul_powers2.asm
@@ -0,0 +1,8 @@
+ADDI    r0, 1,
+ADDI    r1, 2,
+MUL     r0, r1  # product 2
+MUL     r1, r0  # product 4
+MUL     r0, r1  # product 8
+MUL     r1, r0  # product 32
+MUL     r0, r1  # product 256
+MUL     r1, r0  # product 8192
