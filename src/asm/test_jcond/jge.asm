62df97a1d4a9c7a22f794b049b73e74a56d03f27
Nate Hansen <77819611+nateute@users.noreply.github.com>
2021-11-16 16:48:51 -0700

    Translated jcond tests and fixed typos

--- src/asm/test_jcond/jge.asm
+++ src/asm/test_jcond/jge.asm
@@ -2,7 +2,7 @@
        ADDI   r14, 4   // int i = 4
 .L1:   ADDI    r0, 1   // x++
        SUBI    r14, 1  // i--
-       CMP     r14, 0  // i - 0, jump if i >= 0;
+       CMPI    r14, 0  // i - 0, jump if i >= 0;
        MOVIL   r9, 2   // .L1
        JGE     r9
        ADD     r1, r0  // r1 should get final value of x, which is 5
--- src/asm/test_jcond/jlt_jgt.asm
+++ src/asm/test_jcond/jlt_jgt.asm
@@ -3,7 +3,7 @@
        MOVIL  r1, 4   // y = 4
        CMPI   r0, 10  // x < 10
        MOVIL  r9, 7   // .L2
-       JGT    7
+       JGT    r9
        MOVIL  r1, 3   // y = 3
 .L2:   CMPI   r1, 4   // y > 4
        MOVIL  r10, 11 // .L3
