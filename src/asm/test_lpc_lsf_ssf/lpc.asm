b44a6029678893dd811e2c5a9bdfbbe159550fcb
Nate Hansen <77819611+nateute@users.noreply.github.com>
2021-11-18 19:18:21 -0700

    finish tests on lpc, lsf, ssf

---
+++ src/asm/test_lpc_lsf_ssf/lpc.asm
@@ -0,0 +1,7 @@
+ADD     r0 r0
+ADD     r0 r0
+ADD     r0 r0
+ADD     r0 r0
+LPC     r12       // load PC + 1 into r12
+MOVIL   r15 0x0A
+STORE   r15 r12   // move r12 into 0x0A
---
+++ src/asm/test_lpc_lsf_ssf/lsf_ssf.asm
@@ -0,0 +1,9 @@
+ADDI    r0 0x6
+ADDI    r1 0x5
+CMP     r0 r1
+LSF     r2
+ADDI    r3 0x0F
+STORE   r3 r2
+ADDI    r5 0x05
+SSF     r5
+
